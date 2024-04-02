8c6c7da90ce037619cc0cfa07c533dd816b8a9a9
Gavin Collier <gavin.mc.collier@gmail.com>
2024-04-01 19:22:10 -0700

    footer test

--- dist/index.js
+++ dist/index.js
@@ -3,31 +3,14 @@ var http = require('http');
 var https = require('https');
 var express = require('express');
 const port = 555;
-// var options = {
-//     key: fs.readFileSync('./ssl/privkey.pem'),
-//     cert: fs.readFileSync('./ssl/fullchain.pem'),
-// };
 const app = express();
-const httpApp = express(); //non-sercure server to redirect all traftic to the app server
 app.use(express.static('public/static'));
 app.use(express.static('public/views'));
 app.use(express.static('public/scripts'));
-// httpApp.get("/*", (req, res) => {
-//     res.redirect("https://gavincollier.net");
-// }); //redirect non-secure trafic
 app.get('/', (req, res) => {
     res.send("Hi Mom!");
 });
-app.get('/testies', (req, res) => {
-    res.send("Hi Dad!");
-});
-// http.createServer(httpApp).listen(80, function () {
-//     console.log("Express http redirect server listening on port 80");
-// });
 var server = http.createServer(app).listen(port, function () {
     console.log("Server started on " + port);
 });
-// var server = https.createServer(options, app).listen(port, function () {
-//     console.log("HTTPS server started on " + port);
-// });
 //# sourceMappingURL=index.js.map
