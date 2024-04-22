e1f7c41ef21448a41e0a72ad92207d39cf47ce7a
Gavin Collier <gavin.mc.collier@gmail.com>
2024-04-22 13:57:54 -0700

    bulit

--- dist/index.js
+++ dist/index.js
@@ -19,7 +19,7 @@ app.get('/contact', (req, res) => {
     res.sendFile(reqPath + "/views/contact.html");
 });
 app.get('/resume', (req, res) => {
-    res.download("Gavin Collier - Resume.pdf");
+    res.download(reqPath + "/static/Gavin Collier - Resume.pdf");
 });
 var server = http.createServer(app).listen(port, function () {
     console.log("Server started on " + port);
