8e750fb02053ffb42e0600bd8287f5dd178cac88
Gavin Collier <gavin.mc.collier@gmail.com>
2024-05-01 09:55:03 -0700

    finished landing

--- public/scripts/navbarSticky.js
+++ public/scripts/navbarSticky.js
@@ -1,10 +1,12 @@
 var navbar = document.getElementById("navbar");
-var navbarReact = document.getElementById("navbar").getBoundingClientRect();
+var navbarReact = navbar.getBoundingClientRect();
+var navParent = navbar.parentElement;
+var navParentReact = navParent.getBoundingClientRect();
 
 window.addEventListener("scroll", function () {
-    console.log("window: " + window.scrollY);
-    console.log("nav: " + navbarReact.height);
-    if (window.scrollY > window.innerHeight - navbarReact.height) {
+    console.log("window: " + window.scrollY + "| nav: " + navbarReact.height);
+    console.log("navParentHeight: " + navParentReact.height);
+    if (window.scrollY > navParentReact.height - navbarReact.height) {
         navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
